155ebf0f1fab4b0ae3a4620193db26ec7a59378b
owenr88 <owenr88@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-08 23:12:55 +0000

    Release 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/simple-contact-forms/tags/1.0.2@1195065 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/public/partials/classes/formconstructor.php
@@ -0,0 +1,167 @@
+<?php
+
+class scf_FormConstructor {
+
+	protected $options;
+	private $fields;
+	public $emailContents;
+	private $valid;
+	private $verified;
+	private $errors;
+	private $completed;
+
+
+	/* 
+	 * Constructor functions
+	 */
+	public function __construct() {
+
+		$this->options = array();
+		$this->fields = array();
+		$this->emailContents = '';
+		$this->valid = false;
+		$this->verified = false;
+		$this->errors = array();
+		$this->completed = false;
+
+	}
+
+
+
+	/* 
+	 * Create the form
+	 */
+	public function init($passedOptions = array()) { 
+
+		// Add all the files required
+		require_once 'content.php';
+		require_once 'email.php';
+		require_once 'fields.php';
+		require_once 'formvalidation.php';
+		require_once 'options.php';
+
+		// Make the passed options an array if it's empty. It sometimes comes through as an empty string.
+		if(!$passedOptions) $passedOptions = array();
+
+
+		/*
+		 * 1. Replace success message placeholders
+		 * 2. Cycle through each field, noting their properties in an array
+		 * 3. Add the JS validation script to the contents
+		 * 4. Check if the form has been completed
+		 * 5. If not completed, determine if button or form should be shown, and create the classes
+		 * 6. If completed, determine if it's valid (recaptcha, contents, etc)
+		 * 7. Show errors if form is not valid
+		 * 8. Set the 'from' details
+		 * 9. Send email if form is valid
+		 */
+
+
+		// Set the classes required
+		$contentClass = new scf_Content();
+		$emailClass = new scf_Email();
+		$optionClass = new scf_Options();
+
+		// Set the options and settings
+		$this->options = $optionClass->get($passedOptions);
+
+		// Return false if there is no form to show. This is if no settings have been completed.
+		if( !$this->options['form'] ) return false;
+
+		// Replace placeholder text in the success message
+		$this->replacePlaceholders();
+
+		// Create array of all fields
+		$this->fields = scf_Fields::getSCFFields($optionClass->inputtedfields, $this->options, false);
+
+		// Don't show the form if there aren't any fields set
+		if( count($this->fields) < 2 ) return false;
+
+		// Add the js script to the form content
+		$contentClass->addValidationScript($this->fields);
+
+		// Check if the form has been completed
+		$this->completed = scf_FormValidation::isFormCompleted($this->fields);
+
+		// Check if the form values are valid been completed. Pass errors if not.
+		$this->valid = scf_FormValidation::isFormValid($this->fields, $this->completed);
+		if(is_array($this->valid) && $this->completed) $this->addError($this->valid);
+
+		// Check if the form verification has passed successfully. Pass errors if not.
+		$this->verified = scf_FormValidation::isFormVerified($this->options, $this->fields, $this->completed);
+		if($this->verified !== true && $this->completed) $this->addError($this->verified);
+
+		// Form is completed. Now check if it's valid and verified
+		if( $this->completed === true && $this->valid === true && $this->verified === true ) {
+
+			// Create the success message
+			$contentClass->addSuccessMessage($this->options);
+
+			// Send the email
+			$emailClass->sendEmail($this->fields, $this->options);
+
+			// Reset the values from the fields
+			$this->fields = array();
+			$this->fields = scf_Fields::getSCFFields($optionClass->inputtedfields, $this->options, true);
+
+		}
+
+		// Add the form or button as required and output the content with errors (if there are any).
+		if( $this->options['btn'] == true ) $contentClass->addButton($this->options);
+        if( $this->options['form'] == true ) $contentClass->addForm($this->options, $this->fields, $this->errors, $this->verified, $this->completed);
+
+		// Echo or return the final page content
+		if( !$this->options['return'] ) echo $contentClass->pageContent;
+		return $contentClass->pageContent;
+
+	}
+
+
+
+	/* 
+	 * Replace the placeholders from the success message
+	 */
+	private function replacePlaceholders() {
+
+		// Replace the {{PREVPAGE}} placeholder
+	    if( !empty($_REQUEST['prevpage']) ) {
+	        $this->options['success_msg'] = str_replace('{{PREVPAGE}}', $_REQUEST['prevpage'], $this->options['success_msg']);
+	    } else {
+	        $this->options['success_msg'] = str_replace('{{PREVPAGE}}', get_permalink(), $this->options['success_msg']);
+	    }
+
+		// Replace the {{HOMEPAGE}} placeholder
+	    $this->options['success_msg'] = str_replace('{{HOMEPAGE}}', get_bloginfo('url'), $this->options['success_msg']);
+
+		// Replace the {{HOMEPAGE}} placeholder
+	    $this->options['success_msg'] = str_replace('{{PAGETITLE}}', get_the_title(), $this->options['success_msg']);
+
+	}
+
+
+
+	/* 
+	 * Check if the form values are valid
+	 */
+	public function addError($message) {
+
+		// Add error to the array
+		if( is_array($message) ) {
+
+			// If it's an array being passed, pass the new messages to it
+			$this->errors = array_merge($message);
+
+		} else {
+
+			// Otherwise pass the new message
+			$this->errors[] = $message;
+
+		}
+
+	}
+
+
+
+}
+
+?>
---
+++ trunk/public/partials/classes/options.php
@@ -0,0 +1,160 @@
+<?php
+
+class scf_Options {
+
+	private $defaultSettings;
+	private $defaultOptions;
+	public $inputtedfields;
+	private $options;
+	private $passedOptions;
+
+
+	/* 
+	 * Constructor functions
+	 */
+	public function __construct() {
+
+		// Default settings to use in the beginning
+		$this->defaultSettings = array();
+
+		// Default options to use in the beginning
+		$this->defaultOptions = array(
+	        'form_styling'      => 'full-width',                    // Is the form 'full-width' or 'narrow'
+	        'return'            => false,                           // Return the contents or echo
+	        'column_class'      => 'col-sm-12',                     // Split form over two columns
+	        'labels'            => true,                            // Use labels
+	        'placeholders'      => false,                           // Use placeholders
+	    );
+
+		// Dummy field data
+		$this->inputtedfields = maybe_unserialize( get_option( 'scf_table_fields', array() ) );
+
+		// Final options to return
+		$this->options = array();
+
+	}
+
+
+	/* 
+	 * return the options
+	 */
+	private function replaceWithPassed() {
+
+		// Use a more managable variable
+		$o = $this->passedOptions;
+
+		// Is the form styling option set
+		if( isset($o['width']) ) $this->defaultOptions['form_styling'] = $o['width'];
+
+		// Should the form be returned
+		if( isset($o['return']) ) $this->defaultOptions['return'] = $o['return'];
+
+		// Change if a button is required
+		if( isset($o['button']) ) $this->defaultOptions['btn'] = $o['button'];
+
+		// Change if the form is collapsed
+		if( isset($o['form_collapse']) ) $this->defaultOptions['form_collapse'] = $o['form_collapse'];
+
+		// Change the button text if required
+		if( isset($o['btn_text']) ) $this->defaultOptions['btn_text'] = $o['btn_text'];
+
+		// Change the form title if required
+		if( isset($o['form_title']) ) $this->defaultOptions['form_title'] = isset($_POST['form_title']) ? $_POST['form_title'] : $o['form_title'];
+
+		// Change the email subject
+		if( isset($o['email_subject']) ) $this->defaultOptions['email_subject'] = isset($_POST['email_subject']) ? $_POST['email_subject'] : $o['email_subject'];
+
+	}
+
+
+	/* 
+	 * return the options
+	 */
+	private function setDefaults() {
+
+		$arr = array();
+
+		// Use a form?
+		$arr['form'] = get_option('scf_form', false);
+
+		// Location for the form to be sent to
+		$arr['send_to_url'] = get_permalink();
+
+		// Get the form title
+		$arr['form_title'] = get_option('scf_form_title', '<h2>Enquire now!</h2>');
+
+		// Get the email subject
+		$arr['email_subject'] = get_option('scf_email_subject', 'Website Enquiry');
+
+		// Get the email recipients
+		$arr['email_recipients'] = get_option('scf_email_recipients', get_bloginfo('admin_email'));
+
+		// What form styling should be used?
+		$arr['form_styling'] = get_option('scf_form_styling', 'bootstrap');
+
+		// Does the Bootstrap CDN need to be included?
+		$arr['include_bootstrap'] = get_option('scf_include_bootstrap', ($arr['form_styling'] == 'bootstrap' ? true : false) );
+
+		// Does the FontAwesome CDN need to be included?
+		$arr['include_fontawesome'] = get_option('scf_include_fontawesome', ($arr['form_styling'] == 'bootstrap' ? true : false) );
+
+		// Get the success message
+		$arr['success_msg'] = get_option('scf_success_msg', 'Thanks!');
+
+		// Use reCAPTCHA or maths test
+		$arr['validation'] = get_option('scf_validation', 'maths');
+
+		// Use a button?
+		$arr['btn'] = (get_option('scf_display_button') == 'true' ? true : false);
+
+		// Collapse the form
+		$arr['form_collapsed'] = (get_option('scf_default_collapse') == 'true' && $arr['btn'] ? true : false);
+
+		// Get the text for the button
+		$arr['btn_text'] = get_option('scf_button_text', 'Get in touch now');
+
+		// Get the button icon side
+		$arr['btn_icon_side'] = get_option('scf_button_side', 'left');
+
+		// Get the button icon (FontAwesome)
+		$arr['btn_icon_type'] = get_option('scf_button_icon', 'fa-comments');
+
+		// Get the public key for reCAPTCHA
+		$arr['public_key'] = get_option('scf_recaptcha_public', '');
+
+		// Get the private key for reCAPTCHA
+		$arr['private_key'] = get_option('scf_recaptcha_private', '');
+
+		// Set the final default settings
+		$this->defaultSettings = $arr;
+
+	}
+
+
+	/* 
+	 * return the options
+	 */
+	public function get($passedOptions) {
+
+		// Set the passed options in frontend or shortcodes
+		$this->passedOptions = $passedOptions;
+
+		// Set the default options from the database
+		$this->setDefaults();
+
+		// Replace any defaults with passed options
+		$this->replaceWithPassed();
+
+		// Merge the options
+		$this->options = $this->defaultOptions + $this->defaultSettings;
+
+		// Return the options
+		return $this->options;
+
+	}
+
+
+
+
+
+}
